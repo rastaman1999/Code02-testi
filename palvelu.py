838895dca21c08276716ef3dccd1d0b3e0d1c9e2
Anton Skibjuk <Anton.Skibjuk@edu.mayk.fi>
2025-03-14 11:54:03 +0200

    painikkeen lisäys

--- palvelu.py
+++ palvelu.py
@@ -1,8 +1,9 @@
 from wsgiref.simple_server import make_server
 
 def app(environ, respond):
-	respond('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-	yield "Hello w€rld😞!".encode('utf-8')
+    respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
+    yield "Morjesta w€rld😞!".encode('utf-8')
+    yield "<form method=GET><input type=button value=paina></form>".encode("utf-8")
 
 if __name__ == '__main__':
 	with make_server("localhost", 8000, app) as server: 
